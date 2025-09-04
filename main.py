edac1a2b2f9c61c4d35654cc6994b2058167f256
Stella <aniretake0910@gmail.com>
2025-09-04 13:38:38 +0300

    Add counting of all and active passes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
-
 import django
+from dotenv import load_dotenv
 
+
+load_dotenv()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
@@ -9,4 +11,11 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+
+    passcards = Passcard.objects.all()
+    some_passcard = passcards[0]
+
+    active_passcards = Passcard.objects.filter(is_active=True)
+
+    print('Всего пропусков:', Passcard.objects.count())  # noqa: T001
+    print("Активных пропусков:", len(active_passcards))
