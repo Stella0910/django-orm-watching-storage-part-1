96baf36c0388730dede14e1c0f90e395d31a16f0
Stella <aniretake0910@gmail.com>
2025-09-04 13:36:18 +0300

    Add environment variables

--- settings.py
+++ settings.py
@@ -1,17 +1,19 @@
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'HOST': os.getenv('HOST_BD'),
+        'PORT': '5434',
+        'NAME': os.getenv('NAME_BD'),
+        'USER': os.getenv('USER_BD'),
+        'PASSWORD': os.getenv('PASSWORD_BD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.getenv('SECRET_KEY_BD')
 
 TIME_ZONE = 'Europe/Moscow'
 
